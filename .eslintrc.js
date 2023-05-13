1cb2a03677ce3851a6d77473230c1ce518335f92
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-05-13 18:44:09 -0300

    Install prettier & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  extends: ["next/core-web-vitals", "plugin:prettier/recommended"],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-var": "error",
+    "no-console": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-var-requires": "error",
+    "@typescript-eslint/no-non-null-assertion": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "react-hooks/rules-of-hooks": "warn",
+    "react-hooks/exhaustive-deps": "error",
+    "prettier/prettier": "error",
+    "@next/next/no-document-import-in-page": "off",
+    "prefer-const": "warn",
+    "prefer-arrow-callback": "warn",
+    "import/no-duplicates": "error",
+    "import/no-unresolved": "off",
+    "import/order": [
+      "error",
+      {
+        groups: [["external", "builtin"], "parent", ["index", "internal", "sibling"]],
+        pathGroups: [
+          {
+            pattern: "react-dom",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        "newlines-between": "never",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
