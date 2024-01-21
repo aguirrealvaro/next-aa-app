9a39792f3b37aa55de52d21541bbb67ffb030eca
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2024-01-20 22:18:54 -0300

    wrapper md

--- src/components/wrapper/wrapper.tsx
+++ src/components/wrapper/wrapper.tsx
@@ -7,7 +7,7 @@ type WrapperProps = {
 };
 
 const Wrapper = ({ children, className }: WrapperProps) => {
-  return <div className={cn("mx-auto w-4/5 max-w-6xl", className)}>{children}</div>;
+  return <div className={cn("mx-auto w-4/5 max-w-6xl xs:w-11/12", className)}>{children}</div>;
 };
 
 export { Wrapper };
