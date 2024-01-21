11f7279c1e01eb02a637007bdf0faf9cd0f3015c
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2024-01-20 22:14:44 -0300

    add links to features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,58 @@
+import { ExternalLink } from "lucide-react";
 import { CopyButton, Wrapper } from "@/components";
 import { cn } from "@/lib/cn";
 
-const features = [
-  { name: "NextJS", description: "React framework to render components on the server" },
+type Feature = {
+  name: string;
+  url?: string;
+  description: string;
+};
+
+const features: Feature[] = [
+  {
+    name: "NextJS",
+    description: "React framework to render components on the server",
+    url: "https://nextjs.org/",
+  },
   {
     name: "App Router",
     description: "New paradigm for building applications using React's latest features",
   },
-  { name: "Typescript", description: "Enhance code quality and maintainability" },
+  {
+    name: "Typescript",
+    description: "Enhance code quality and maintainability",
+  },
   {
     name: "TailwindCSS",
     description: "A utility-first CSS framework for rapid UI development.",
+    url: "https://tailwindcss.com/",
   },
   { name: "Prettier & ESLint", description: "For clean, consistent, and error-free code" },
   {
     name: "Pre commit",
-    description: "Checking linter and types on staged files after commits",
+    description:
+      "Checking linter and types on staged files before commits with husky and lint-staged",
   },
-  { name: "Dark mode", description: "Support for light and dark mode" },
-  { name: "Error Boundary", description: "To catching rendering errors" },
-  { name: "Knip", description: "Find deadcode" },
-  { name: "ts-reset", description: "Extremely strict TypeScript" },
-  { name: "lucide-icons", description: "Beautiful and Consistent icons" },
+  {
+    name: "Dark mode",
+    description: "Support for light and dark mode",
+  },
+  {
+    name: "Error Boundary",
+    description: "To catching rendering errors",
+    url: "https://www.npmjs.com/package/react-error-boundary",
+  },
+  { name: "Knip", description: "Find deadcode", url: "https://github.com/webpro/knip" },
+  {
+    name: "ts-reset",
+    description: "Extremely strict TypeScript",
+    url: "https://www.totaltypescript.com/ts-reset",
+  },
+  {
+    name: "lucide-icons",
+    description: "Beautiful and Consistent icons",
+    url: "https://lucide.dev/guide/packages/lucide-react",
+  },
 ];
 
 export default function Home() {
@@ -43,13 +74,25 @@ export default function Home() {
             <CopyButton />
           </div>
           <div className="grid grid-cols-3 gap-4 text-center md:grid-cols-2 xs:grid-cols-1">
-            {features.map(({ name, description }) => {
+            {features.map(({ name, description, url }) => {
               return (
                 <div
                   key={name}
                   className="flex flex-col gap-4 rounded-lg bg-bg-secondary p-4 shadow"
                 >
-                  <h2 className="font-semibold">{name}</h2>
+                  <h2 className="font-semibold">
+                    {url ? (
+                      <a
+                        href={url}
+                        target="_blank"
+                        className="flex items-center justify-center gap-2 hover:underline"
+                      >
+                        {name} <ExternalLink size={18} />
+                      </a>
+                    ) : (
+                      name
+                    )}
+                  </h2>
                   <p>{description}</p>
                 </div>
               );
